341064f07cda337f2f2b94a7836b17e5a07e3166
Thor138 <tjcourey84@gmail.com>
2021-05-15 17:34:48 -0600

    got questions to loop properly through inquirer,team array defined and being populated correctly

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
-const fs = require("js");
+const fs = require("fs");
 const path = require("path");
-
+const team = [];
 const managerQuestions = [
   {
     type: "input",
-    name: "managerName",
-    message: "Managers Name:",
+    name: "name",
+    message: "Manager's Name:",
   },
   {
     type: "input",
@@ -19,7 +19,12 @@ const managerQuestions = [
   {
     type: "input",
     name: "email",
-    Message: "Email Address:",
+    message: "Email Address:",
+  },
+  {
+    type: "input",
+    name: "number",
+    message: "Office Number:",
   },
 ];
 
@@ -27,7 +32,122 @@ const employeeQuestions = [
   {
     type: "list",
     name: "nextChoice",
-    Message: "Would you like to add additional employees?",
-    choices: ["Engineer", "Intern", "No, my team is complete."],
+    message: "Would you like to add additional employees?",
+    choices: ["Engineer", "Intern", "No"],
   },
 ];
+const engineerQuestions = [
+  {
+    type: "input",
+    name: "name",
+    message: "Engineer's Name:",
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "ID Number:",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Email Address:",
+  },
+  {
+    type: "input",
+    name: "gitHub",
+    message: "GitHub User Name:",
+  },
+];
+const internQuestions = [
+  {
+    type: "input",
+    name: "name",
+    message: "Intern's Name:",
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "ID Number:",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Email Address:",
+  },
+  {
+    type: "input",
+    name: "school",
+    message: "School of Origin:",
+  },
+];
+
+engineerInput = () => {
+  inquirer
+    .prompt([...engineerQuestions, ...employeeQuestions])
+    .then((data) => {
+      team.push(new Engineer(data.name, data.id, data.email, data.gitHub));
+      switch (data.nextChoice) {
+        case "Engineer": {
+          engineerInput();
+          break;
+        }
+        case "Intern": {
+          internInput();
+          break;
+        }
+        case "No": {
+          printHtml(team);
+          break;
+        }
+      }
+    })
+    .catch((err) => console.log(err));
+};
+internInput = () => {
+  inquirer
+    .prompt([...internQuestions, ...employeeQuestions])
+    .then((data) => {
+      team.push(new Intern(data.name, data.id, data.email, data.school));
+      switch (data.nextChoice) {
+        case "Engineer": {
+          engineerInput();
+          break;
+        }
+        case "Intern": {
+          internInput();
+          break;
+        }
+        case "No": {
+          printHtml(team);
+          break;
+        }
+      }
+    })
+    .catch((err) => console.log(err));
+};
+printHtml = (data) => {
+  console.log(data);
+};
+
+listTeam = () => {
+  inquirer
+    .prompt([...managerQuestions, ...employeeQuestions])
+    .then((data) => {
+      team.push(new Manager(data.name, data.id, data.email, data.number));
+      switch (data.nextChoice) {
+        case "Engineer": {
+          engineerInput();
+          break;
+        }
+        case "Intern": {
+          internInput();
+          break;
+        }
+        case "No":
+          printHtml(team);
+      }
+    })
+    .catch((err) => console.log(err));
+};
+
+listTeam();
