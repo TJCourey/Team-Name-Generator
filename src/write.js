1099c58f511ae7d8e038d67fbdfa28a69a0b5a0a
Thor138 <tjcourey84@gmail.com>
2021-05-16 13:07:27 -0600

    fixed minor bug in constructors, first attempt at html generation

---
+++ src/write.js
@@ -0,0 +1,114 @@
+const fs = require("fs");
+
+managerCard = (member) => {
+  const manager = `<div class="card col-3 m-3">
+  <div class="card-header bg-info">
+    <h3>${member.getRole()}</h3>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Name: ${member.getName()}</li>
+    <li class="list-group-item">ID: ${member.getId()}</li>
+    <li class="list-group-item">Email: ${member.getEmail()}</li>
+    <li class="list-group-item">Office Number: ${member.getPhone()}</li>
+  </ul>
+</div>`;
+  return manager;
+};
+engineerCard = (member) => {
+  const engineer = `<div class="card col-3 m-3">
+  <div class="card-header bg-info">
+    <h3>${member.getRole()}</h3>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Name: ${member.getName()}</li>
+    <li class="list-group-item">ID: ${member.getId()}</li>
+    <li class="list-group-item">Email: ${member.getEmail()}</li>
+    <li class="list-group-item">GitHub Username: ${member.getGithub()}</li>
+  </ul>
+</div>`;
+  return engineer;
+};
+internCard = (member) => {
+  const intern = `<div class="card col-3 m-3">
+  <div class="card-header bg-info">
+    <h3>${member.getRole()}</h3>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Name: ${member.getName()}</li>
+    <li class="list-group-item">ID: ${member.getId()}</li>
+    <li class="list-group-item">Email: ${member.getEmail()}</li>
+    <li class="list-group-item">School: ${member.getSchool()}</li>
+  </ul>
+</div>`;
+  return intern;
+};
+
+const writer = (team) => {
+  let employeeCards = "";
+  team.forEach((member) => {
+    switch (member.getRole()) {
+      case "Manager":
+        employeeCards += managerCard(member);
+        break;
+      case "Engineer":
+        employeeCards += engineerCard(member);
+        break;
+      case "Intern":
+        employeeCards += internCard(member);
+        break;
+      default:
+        break;
+    }
+  });
+  const htmlText = `
+  <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x"
+      crossorigin="anonymous"
+    />
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"
+      crossorigin="anonymous"
+    ></script>
+  </head>
+  <body>
+    <header class="container-fluid bg-success p-5">
+      <h1 class="text-center p-3 text-info fs-1 fw-bold">Project Team</h1>
+    </header>
+    <main class="container row">
+      ${employeeCards}
+      </div>
+    </main>
+    <script
+      src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+      integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js"
+      integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT"
+      crossorigin="anonymous"
+    ></script>
+  </body>
+</html>
+  `;
+};
+
+// console.log(writer(team));
+
+fileExport = (team) => {
+  fs.writeFile("./index.html", writer(team), (err) => {
+    err ? console.log(err) : console.log("Generating");
+  });
+};
+
+fileExport(team);
